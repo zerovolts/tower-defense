9f029d89871d96c84edbc51baf8ede7a4a366610
Zach Stone <zach@zerovolts.com>
2022-04-06 02:48:16 -0400

    Add regular polygon mesh

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,88 @@
+use bevy::{
+    prelude::*,
+    render::{mesh::Indices, render_resource::PrimitiveTopology},
+    sprite::Mesh2dHandle,
+};
+
+use std::f32::consts::TAU;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .insert_resource(ClearColor(Color::rgb(0.2, 0.2, 0.2)))
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(setup)
+        .run();
+}
+
+fn setup(
+    mut commands: Commands,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    mut meshes: ResMut<Assets<Mesh>>,
+) {
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+
+    commands.spawn_bundle(ColorMesh2dBundle {
+        mesh: Mesh2dHandle(meshes.add(RegPoly::new(6, 16.0).into())),
+        material: materials.add(Color::rgb(1.0, 0.5, 0.0).into()),
+        transform: Transform::from_xyz(0.0, 0.0, 0.0),
+        ..Default::default()
+    });
+}
+
+#[derive(Clone, Copy)]
+struct RegPoly {
+    pub sides: u32,
+    pub radius: f32,
+}
+
+impl RegPoly {
+    fn new(sides: u32, radius: f32) -> Self {
+        Self { sides, radius }
+    }
+}
+
+impl From<RegPoly> for Mesh {
+    fn from(polygon: RegPoly) -> Self {
+        let mut mesh = Mesh::new(PrimitiveTopology::TriangleList);
+
+        let mut positions = Vec::with_capacity(polygon.sides as usize + 1);
+        positions.push([0.0, 0.0, 0.0]);
+        for i in 0..polygon.sides {
+            let angle = (i as f32) / (polygon.sides as f32) * TAU;
+            dbg!(angle);
+            positions.push([
+                angle.cos() * polygon.radius,
+                angle.sin() * polygon.radius,
+                0.0,
+            ]);
+        }
+
+        let normals = vec![[0.0, 0.0, 1.0]; (polygon.sides + 1) as usize];
+
+        let uvs = positions
+            .iter()
+            .map(|v| {
+                [
+                    v[0] / polygon.radius / 2.0 + 0.5,
+                    v[1] / polygon.radius / 2.0 + 0.5,
+                ]
+            })
+            .collect::<Vec<[f32; 2]>>();
+
+        let mut indices = Vec::with_capacity(polygon.sides as usize * 3);
+        for i in 1..(polygon.sides) {
+            indices.push(0);
+            indices.push(i);
+            indices.push(i + 1);
+        }
+        indices.push(0);
+        indices.push(polygon.sides);
+        indices.push(1);
+
+        mesh.set_attribute(Mesh::ATTRIBUTE_POSITION, positions);
+        mesh.set_attribute(Mesh::ATTRIBUTE_NORMAL, normals);
+        mesh.set_attribute(Mesh::ATTRIBUTE_UV_0, uvs);
+        mesh.set_indices(Some(Indices::U32(indices)));
+        mesh
+    }
 }
